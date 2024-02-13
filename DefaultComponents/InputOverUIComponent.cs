c78dea6272b2adfeeadb46de97c8751ecbb9ba7f
danila.golovenko <danila.g@playstrom.io>
2024-02-13 09:31:48 +0300

    upd: input over ui logic

--- DefaultComponents/InputOverUIComponent.cs
+++ DefaultComponents/InputOverUIComponent.cs
@@ -1,15 +1,40 @@
 using System;
+using System.Collections.Generic;
 using HECSFramework.Core;
 using HECSFramework.Unity;
-using Systems;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
 
 namespace Components
 {
-    [RequiredAtContainer(typeof(InputOverUISystem))]
     [Serializable][Documentation(Doc.HECS, Doc.Input, "this component holds bool  - on this moment we over ui or not")]
-    public sealed partial class InputOverUIComponent : BaseComponent, IWorldSingleComponent
+    public sealed partial class InputOverUIComponent : BaseComponent, IWorldSingleComponent, IInitable
     {
-        public bool InputOverUI;
         public InputIdentifier TouchPositionInputIdentifier;
+        
+        private PointerEventData pointerEventData;
+        private List<RaycastResult> raycastResults = new List<RaycastResult>(3);
+        private InputAction touchAction;
+        private bool inputOverUI;
+        private int frameCount;
+        public void Init()
+        {
+            pointerEventData  = new PointerEventData(EventSystem.current);
+            EntityManager.Default.GetSingleComponent<InputActionsComponent>()
+                .TryGetInputAction(TouchPositionInputIdentifier.name, out touchAction);
+        }
+        
+        public bool IsOverUI()
+        {
+            if (Time.frameCount == frameCount)
+                return inputOverUI;
+            
+            pointerEventData.position = touchAction.ReadValue<Vector2>();
+            EventSystem.current.RaycastAll(pointerEventData, raycastResults);
+            inputOverUI = raycastResults.Count > 0;
+            frameCount = Time.frameCount;
+            return inputOverUI;
+        }
     }
 }
